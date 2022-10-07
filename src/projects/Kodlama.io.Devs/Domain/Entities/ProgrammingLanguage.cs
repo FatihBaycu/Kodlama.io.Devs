da49e90af12f88e85ce93c6a68ed504dc716c8b1
FatihBaycu <email@example.com>
2022-10-07 15:55:42 +0300

    first commit

---
+++ src/projects/Kodlama.io.Devs/Domain/Entities/ProgrammingLanguage.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProgrammingLanguage : Entity
+    {
+        public ProgrammingLanguage()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public ProgrammingLanguage(int Id,string name)
+        {
+            Id = Id;
+            Name = name;
+        }
+    }
+}
